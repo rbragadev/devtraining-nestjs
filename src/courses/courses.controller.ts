ddfeadbef0054bdc27e83847efc358aa94198e17
Raphael Braga <raffael.info@gmail.com>
2022-12-21 17:20:26 -0300

    Module

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Delete, Patch } from '@nestjs/common/decorators';
+
+@Controller('courses')
+export class CoursesController {
+  @Get()
+  findAll(@Res() response) {
+    return response.status(200).send('Listagem de cursos');
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Curso #${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body('name') body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `Atualizaçao do Curso #${id}`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Exclusao do Curso #${id}`;
+  }
+}
